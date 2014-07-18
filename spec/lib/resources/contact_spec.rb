1ae789b871fb0c164e5a1dfac8048d600f9335c2
Willian van der Velde <mail@willian.io>
2014-07-18 11:35:54 +0200

    Do some real contact testing

--- spec/lib/resources/contact_spec.rb
+++ spec/lib/resources/contact_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe SpreeMoneybird::Contact do
   let(:order) do
     order = create :order
-    # Prevent 422 (duplicate customer id)
-    order.user.stub(:id) { SecureRandom.uuid }
+    order.user.stub(:id) { SecureRandom.uuid  }# Prevents 422 (duplicate customer id)
+
     order
   end
 
@@ -13,33 +13,25 @@ describe SpreeMoneybird::Contact do
       self.site = "https://#{ENV['MONEYBIRD_COMPANY']}.moneybird.nl"
       self.user = ENV['MONEYBIRD_USER']
       self.password = ENV['MONEYBIRD_PASSWORD']
-      if ENV['PROXY']
-        self.proxy = ENV['PROXY']
-      end
-
-
+      self.proxy = ENV['PROXY'] if ENV['PROXY']
     end
   end
 
-  let(:contact) { SpreeMoneybird::Contact.from_order(order) }
+  subject { SpreeMoneybird::Contact.from_order(order) }
 
   describe 'create an contact object from an order' do
-    subject { contact }
-
     it 'has the email address of the order\'s user' do
       expect(subject.email).to eql(order.email)
     end
   end
 
   describe 'saving a contact' do
-    subject { puts contact; }
+    before do
+      subject.save
+    end
 
-    it 'saved the contact' do
-      puts contact.new?
-      puts contact.errors.inspect
-      puts contact.save
-      puts contact.errors.inspect
-      #subject
+    it 'assigns the id' do
+      expect(subject.id).not_to be_nil
     end
   end
 end

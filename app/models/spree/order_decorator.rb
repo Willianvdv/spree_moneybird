db465a1e49190adc48bf6fec60ce6c289b5460a0
Willian van der Velde <mail@willian.io>
2014-07-28 15:53:55 +0200

    Order work

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,33 +1,27 @@
 Spree::Order.class_eval do
   after_save :sync_with_moneybird, if: :complete?
-  after_save :send_moneybird_invoice, if: proc { |o| o.shipment_state == 'shipped' }
+  after_save :send_moneybird_invoice
 
   private
 
   def send_moneybird_invoice
+    return unless self.shipment_state == 'shipped'
+
     SpreeMoneybird::Invoice.send_invoice(self)
+    SpreeMoneybird::Payment.create_payment_from_payment(payments.last)
+  rescue Exception => e
+    Appsignal.add_exception(e) if defined? Appsignal
+    raise e
   end
 
   def sync_with_moneybird
     sync_moneybird_contact
     sync_moneybird_invoice
-    sync_moneybird_payment
-  end
-
-  def sync_moneybird_payment
-    return unless moneybird_id.nil?
-    return unless payment = payments.last
-    SpreeMoneybird::Payment.create_payment_from_payment(payment)
-
-  rescue Exception => e
-    Rails.logger.error e
-    Appsignal.add_exception(e) if defined? Appsignal
   end
 
   def sync_moneybird_contact
     return unless user && user.moneybird_id.nil?
     SpreeMoneybird::Contact.create_contact_from_order(self)
-
   rescue Exception => e
     Rails.logger.error e
     Appsignal.add_exception(e) if defined? Appsignal
@@ -36,7 +30,6 @@ Spree::Order.class_eval do
   def sync_moneybird_invoice
     return unless moneybird_id.nil?
     SpreeMoneybird::Invoice.create_invoice_from_order(self)
-
   rescue Exception => e
     Rails.logger.error e
     Appsignal.add_exception(e) if defined? Appsignal

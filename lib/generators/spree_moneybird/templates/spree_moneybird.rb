80bdbb64e2ea7e87021f499da5e80fea417f3fd7
Rein Aris <info@reinaris.nl>
2014-10-30 14:29:55 +0100

    Added a config setting for the reversed charge tax id

--- lib/generators/spree_moneybird/templates/spree_moneybird.rb
+++ lib/generators/spree_moneybird/templates/spree_moneybird.rb
@@ -2,4 +2,7 @@ SpreeMoneybird.setup do |config|
   config.company = 'your_moneybird_company' # the company in company.moneybird.nl
   config.api = 'your_moneybird_username'
   config.password = 'your_moneybird_password'
+
+  # Verkoopfacturen > BTW verlegd Geen BTW op factuur NL/1e
+  config.reversed_charge_tax_id = 'your_moneybird_reversed_charge_tax_id'
 end
